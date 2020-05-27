41844347e80015f4e7d53c7ba78bde46f2c57f2f
jensencode <55578242+jensencode@users.noreply.github.com>
2020-05-27 22:57:54 +0200

    Code updated

--- eksamenskode/build-a-pc/javascript.js
+++ eksamenskode/build-a-pc/javascript.js
@@ -14,156 +14,148 @@ $(document).ready(function(){
 
 
 	
-// 	read more buttons image change 
 
-	// BUTTON IN SECTION 1
 
-var b1 = document.getElementById('b1')
-var iT = 'f'
-function hideImage() {
-	var image = document.getElementById('b1');
-	
-	if(iT=='f'){
-		image.src = 'images/top-fold.svg';
-		iT = 't';
-	}else {
-		image.src = 'images/bottom-fold.svg';
-		iT = 'f';
+// hover effect read more
+
+var b1c = document.getElementById('b1')
+	function color() {
+		var change = document.getElementById('b1');
+		
+		change.src = 'images/bottom-fold-hover.svg';
+		}
+	function normal(){
+		var unchange = document.getElementById('b1');
+		
+		unchange.src = 'images/bottom-fold.svg'
 	}
 	
-}
 
-b1.addEventListener('click', hideImage);
+b1c.addEventListener('mouseenter',color)
+b1c.addEventListener('mouseout',normal)
 
-// BUTTON IN SECTION 2
+// READ MORE 2
 
-var b2 = document.getElementById('b2')
-var iT2 = 'f'
-function hideImage2() {
-	var image = document.getElementById('b2');
-	
-	if(iT2=='f'){
-		image.src = 'images/top-fold.svg';
-		iT2 = 't';
-	}else {
-		image.src = 'images/bottom-fold.svg';
-		iT2 = 'f';
+var b2c = document.getElementById('b2')
+	function color2() {
+		var change2 = document.getElementById('b2');
+		
+		change2.src = 'images/bottom-fold-hover.svg';
+		}
+	function normal2(){
+		var unchange2 = document.getElementById('b2');
+		
+		unchange2.src = 'images/bottom-fold.svg'
 	}
 	
-}
 
-b2.addEventListener('click', hideImage2);
+b2c.addEventListener('mouseenter',color2)
+b2c.addEventListener('mouseout',normal2)
 
-// BUTTON IN SECTION 3
+// READ MORE 3
 
-var b3 = document.getElementById('b3')
-var iT3 = 'f'
-function hideImage3() {
-	var image = document.getElementById('b3');
-	
-	if(iT3=='f'){
-		image.src = 'images/top-fold.svg';
-		iT3 = 't';
-	}else {
-		image.src = 'images/bottom-fold.svg';
-		iT3 = 'f';
+var b3c = document.getElementById('b3')
+	function color3() {
+		var change3 = document.getElementById('b3');
+		
+		change3.src = 'images/bottom-fold-hover.svg';
+		}
+	function normal3(){
+		var unchange3 = document.getElementById('b3');
+		
+		unchange3.src = 'images/bottom-fold.svg'
 	}
 	
-}
 
-b3.addEventListener('click', hideImage3);
+b3c.addEventListener('mouseenter',color3)
+b3c.addEventListener('mouseout',normal3)
 
-// BUTTON IN SECTION 4
+// READ MORE 4
 
-var b4 = document.getElementById('b4')
-var iT4 = 'f'
-function hideImage4() {
-	var image = document.getElementById('b4');
-	
-	if(iT4=='f'){
-		image.src = 'images/top-fold.svg';
-		iT4 = 't';
-	}else {
-		image.src = 'images/bottom-fold.svg';
-		iT4 = 'f';
+var b4c = document.getElementById('b4')
+	function color4() {
+		var change4 = document.getElementById('b4');
+		
+		change4.src = 'images/bottom-fold-hover.svg';
+		}
+	function normal4(){
+		var unchange4 = document.getElementById('b4');
+		
+		unchange4.src = 'images/bottom-fold.svg'
 	}
 	
-}
 
-b4.addEventListener('click', hideImage4);
+b4c.addEventListener('mouseenter',color4)
+b4c.addEventListener('mouseout',normal4)
 
-// BUTTON IN SECTION 5
+// READ MORE 5
 
-var b5 = document.getElementById('b5')
-var iT5 = 'f'
-function hideImage5() {
-	var image = document.getElementById('b5');
-	
-	if(iT5=='f'){
-		image.src = 'images/top-fold.svg';
-		iT5 = 't';
-	}else {
-		image.src = 'images/bottom-fold.svg';
-		iT5 = 'f';
+var b5c = document.getElementById('b5')
+	function color5() {
+		var change5 = document.getElementById('b5');
+		
+		change5.src = 'images/bottom-fold-hover.svg';
+		}
+	function normal5(){
+		var unchange5 = document.getElementById('b5');
+		
+		unchange5.src = 'images/bottom-fold.svg'
 	}
 	
-}
 
-b5.addEventListener('click', hideImage5);
+b5c.addEventListener('mouseenter',color5)
+b5c.addEventListener('mouseout',normal5)
 
-// BUTTON IN SECTION 6
+// READ MORE 6
 
-var b6 = document.getElementById('b6')
-var iT6 = 'f'
-function hideImage6() {
-	var image = document.getElementById('b6');
-	
-	if(iT6=='f'){
-		image.src = 'images/top-fold.svg';
-		iT6 = 't';
-	}else {
-		image.src = 'images/bottom-fold.svg';
-		iT6 = 'f';
+var b6c = document.getElementById('b6')
+	function color6() {
+		var change6 = document.getElementById('b6');
+		
+		change6.src = 'images/bottom-fold-hover.svg';
+		}
+	function normal6(){
+		var unchange6 = document.getElementById('b6');
+		
+		unchange6.src = 'images/bottom-fold.svg'
 	}
 	
-}
 
-b6.addEventListener('click', hideImage6);
+b6c.addEventListener('mouseenter',color6)
+b6c.addEventListener('mouseout',normal6)
 
-// BUTTON IN SECTION 7
+// READ MORE 7
 
-var b7 = document.getElementById('b7')
-var iT7 = 'f'
-function hideImage7() {
-	var image = document.getElementById('b7');
-	
-	if(iT7=='f'){
-		image.src = 'images/top-fold.svg';
-		iT7 = 't';
-	}else {
-		image.src = 'images/bottom-fold.svg';
-		iT7 = 'f';
+var b7c = document.getElementById('b7')
+	function color7() {
+		var change7 = document.getElementById('b7');
+		
+		change7.src = 'images/bottom-fold-hover.svg';
+		}
+	function normal7(){
+		var unchange7 = document.getElementById('b7');
+		
+		unchange7.src = 'images/bottom-fold.svg'
 	}
 	
-}
 
-b7.addEventListener('click', hideImage7);
+b7c.addEventListener('mouseenter',color7)
+b7c.addEventListener('mouseout',normal7)
 
-// BUTTON IN SECTION 8
+// READ MORE 8
 
-var b8 = document.getElementById('b8')
-var iT8 = 'f'
-function hideImage8() {
-	var image = document.getElementById('b8');
-	
-	if(iT8=='f'){
-		image.src = 'images/top-fold.svg';
-		iT8 = 't';
-	}else {
-		image.src = 'images/bottom-fold.svg';
-		iT8 = 'f';
+var b8c = document.getElementById('b8')
+	function color8() {
+		var change8 = document.getElementById('b8');
+		
+		change8.src = 'images/bottom-fold-hover.svg';
+		}
+	function normal8(){
+		var unchange8 = document.getElementById('b8');
+		
+		unchange8.src = 'images/bottom-fold.svg'
 	}
 	
-}
 
-b8.addEventListener('click', hideImage8);
+b8c.addEventListener('mouseenter',color8)
+b8c.addEventListener('mouseout',normal8)
